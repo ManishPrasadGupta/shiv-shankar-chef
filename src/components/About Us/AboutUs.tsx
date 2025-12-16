8c0370565c9e267d20b49be944263553bb77a5a3
Manish Prasad Gupta <manishprasad7173@gmail.com>
2025-12-16 17:10:22 +0530

    feat: add About Us section with animations and update contact information

--- src/components/About Us/AboutUs.tsx
+++ src/components/About Us/AboutUs.tsx
@@ -1,5 +1,12 @@
+import AboutUsSection from "../ui-elements/AboutUs/about-us";
+
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <>
+      <div className="w-full h-6 bg-linear-to-b from-white to-[#f3f3f0] pointer-events-none" />
+      <AboutUsSection />;
+    </>
+  );
 };
 
 export default AboutUs;
